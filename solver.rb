94cd69e5dad01dd08818cf45d7bc7fd0b4443ff4
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-11-26 15:34:02 -0800

    Fix linters errors

--- solver.rb
+++ solver.rb
@@ -1,27 +1,27 @@
 class Solver
-    def factorial(num)
-      if num.zero?
-        1
-      elsif num.positive?
-        num * factorial(num - 1)
-      else
-        'Oh, there is an error. Please use a positive number'
-      end
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.positive?
+      num * factorial(num - 1)
+    else
+      'Oh, there is an error. Please use a positive number'
     end
+  end
 
-    def reverser(str)
-        str.reverse
-      end
-      def fizzbuzz(num)
-        if num.modulo(3).zero? && num.modulo(5).zero?
-          'fizzbuzz'
-        elsif num.modulo(3).zero?
-          'fizz'
-        elsif num.modulo(5).zero?
-          'buzz'
-        else
-          num.to_s
-        end
-      end
+  def reverser(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if num.modulo(3).zero? && num.modulo(5).zero?
+      'fizzbuzz'
+    elsif num.modulo(3).zero?
+      'fizz'
+    elsif num.modulo(5).zero?
+      'buzz'
+    else
+      num.to_s
     end
-    
+  end
+end
